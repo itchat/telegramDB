ce722e50aaf535d65f5bfb362c95f459726e5d51
ronin <itchat@pm.me>
2023-02-08 12:16:41 +0800

    set env

--- config.py
+++ config.py
@@ -1,19 +1,20 @@
-from pymysql.cursors import DictCursor
+import os
 import pymysql
+from pymysql.cursors import DictCursor
 
 db_config = {
     'host': 'localhost',
-    'user': 'username',
-    'password': 'password',
-    'db': 'db',
+    'user': os.getenv("HOST", "root"),
+    'password': os.getenv("PASS", "123456"),
+    'db': os.getenv("DB", "tgdb"),
     # Change the config data above
     'charset': 'utf8mb4',
     'cursorclass': DictCursor
 }
 
 # user_id: int
-authorized = [123456]
+authorized = [int(os.getenv("ID"))]
 
-token = 'bot_token'
+token = os.getenv("TOKEN")
 
 conn = pymysql.connect(**db_config)
