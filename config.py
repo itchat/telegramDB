c7b9d1045d935ece0c8cd7f723c158a37f05425c
ronin <itchat@pm.me>
2023-02-24 10:36:48 +0800

    Change ConnectionPool to DButils and understood pool object and connection release method

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 import os
-import pymysqlpool
+import pymysql
 from pymysql.cursors import DictCursor
 
-pool = pymysqlpool.ConnectionPool(
-    host='127.0.0.1',
-    user=os.getenv('USER', 'root'),
-    password=os.getenv("PASS", "123456"),
-    db=os.getenv("DB", "tgdb"),
-    port=3306,
-    maxsize=5,
-    charset='utf8mb4',
-    cursorclass=DictCursor,
-    autocommit=True
-)
+DB_CONFIG = {
+    "creator": pymysql,
+    "host": "127.0.0.1",
+    "user": os.getenv('USER', 'root'),
+    "password": os.getenv("PASS", "123456"),
+    "database": os.getenv("DB", "tgdb"),
+    "port": 3306,
+    "charset": "utf8mb4",
+    "maxconnections": 5,
+    "cursorclass": DictCursor,
+    "autocommit": True
+}
 
 # user_id: int
 authorized = [int(os.getenv("ID"))]
--- db/database.py
+++ db/database.py
@@ -1,54 +1,53 @@
 from tools.logger import logger
-from config import pool
+from config import DB_CONFIG
+from dbutils.pooled_db import PooledDB
 
 
 class Database:
     def __init__(self):
-        self.conn = pool.get_connection()
+        self.pool = PooledDB(**DB_CONFIG)
+
+    def _get_connection(self):
+        return self.pool.connection()
 
     def save_to_database(self, text):
         try:
-            with self.conn.cursor() as cursor:
-                sql = "INSERT INTO message (text) VALUES (%s)"
-                cursor.execute(sql, (text,))
+            with self._get_connection() as conn:
+                with conn.cursor() as cursor:
+                    sql = "INSERT INTO message (text) VALUES (%s)"
+                    cursor.execute(sql, (text,))
         except Exception as e:
             logger.error("Error saving to database: %s", e, exc_info=True)
-        # 线程回收到线程池
-        finally:
-            self.conn.close()
 
     def search_in_database(self, text):
         try:
-            with self.conn.cursor() as cursor:
-                text = "%{}%".format(text)
-                sql = "SELECT id, text FROM message WHERE TEXT LIKE %s"
-                cursor.execute(sql, (text,))
-                result = cursor.fetchall()
-                return result
+            with self._get_connection() as conn:
+                with conn.cursor() as cursor:
+                    text = "%{}%".format(text)
+                    sql = "SELECT id, text FROM message WHERE TEXT LIKE %s"
+                    cursor.execute(sql, (text,))
+                    result = cursor.fetchall()
+                    return result
         except Exception as e:
             logger.error("Error searching in database: %s", e, exc_info=True)
-        finally:
-            self.conn.close()
 
     def delete_record(self, record_id):
         try:
-            with self.conn.cursor() as cursor:
-                sql = "DELETE FROM message WHERE id = %s"
-                cursor.execute(sql, (record_id,))
+            with self._get_connection() as conn:
+                with conn.cursor() as cursor:
+                    sql = "DELETE FROM message WHERE id = %s"
+                    cursor.execute(sql, (record_id,))
         except Exception as e:
             logger.error("Error delete in database: %s", e, exc_info=True)
-        finally:
-            self.conn.close()
 
     def read_database(self):
         try:
-            with self.conn.cursor() as cursor:
-                # RAND() generates a random value for each row in the table
-                sql = "SELECT id, text FROM message ORDER BY rand() LIMIT 10"
-                cursor.execute(sql)
-                result = cursor.fetchall()
-                return result
+            with self._get_connection() as conn:
+                with conn.cursor() as cursor:
+                    # RAND() generates a random value for each row in the table
+                    sql = "SELECT id, text FROM message ORDER BY rand() LIMIT 10"
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    return result
         except Exception as e:
             logger.error("Error read 10 random message in database: %s", e, exc_info=True)
-        finally:
-            self.conn.close()
